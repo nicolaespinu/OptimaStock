e53f11a4ebd67a225eb67369338cc6113ee2084f
Nicolae Spinu <11spinic@gmail.com>
2023-08-25 22:01:02 +0300

    #1 Added License CRUD operations + locale

--- src/main/java/com/optima/OptimaApplication.java
+++ src/main/java/com/optima/OptimaApplication.java
@@ -2,6 +2,12 @@ package com.optima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class OptimaApplication {
@@ -10,4 +16,18 @@ public class OptimaApplication {
         SpringApplication.run(OptimaApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.addBasenames("messages");
+        return messageSource;
+    }
 }
---
+++ src/main/java/com/optima/license/controller/LicenseController.java
@@ -0,0 +1,48 @@
+package com.optima.license.controller;
+
+import com.optima.license.model.License;
+import com.optima.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    LicenseService licenseService;
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request,
+            Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    }
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+}
