b00809aeece55a5230e1209bcbc2f8842342eae7
Nicolae Spinu <11spinic@gmail.com>
2023-08-25 19:54:06 +0300

    init commit

---
+++ src/main/java/com/optima/OptimaApplication.java
@@ -0,0 +1,13 @@
+package com.optima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optima/OptimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
